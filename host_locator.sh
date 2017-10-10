f1293d9a53da9099bb72b192d355955acd3f35bf
Javier Faustino <faustino.javier34@gmail.com>
2017-10-10 14:13:02 -0500

    Updated host_locator.sh
    Added:
    - Color syntax for ip address found
    - Script Logo

--- host_locator.sh
+++ host_locator.sh
@@ -1,59 +1,56 @@
 #! /bin/bash
 
 ############################################################
-# Author: c0defire
+# Author: C0defire
 # Version 1.0
 # This script will find all live hosts in a Class C range.
-# 
-# RED TEAM TOOLS
 ############################################################
 
+###### Constants ######
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+#######################
+
 clear
-# Put bad-ass banner here
 cat << "EOF"
-.__                    __     .__                        __                
-|  |__   ____  _______/  |_   |  |   ____   ____ _____ _/  |_  ___________ 
-|  |  \ /  _ \/  ___/\   __\  |  |  /  _ \_/ ___\\__  \\   __\/  _ \_  __ \
-|   Y  (  <_> )___ \  |  |    |  |_(  <_> )  \___ / __ \|  | (  <_> )  | \/
-|___|  /\____/____  > |__|____|____/\____/ \___  >____  /__|  \____/|__|   
-     \/           \/    /_____/                \/     \/                   
-                                                                              V 1.0
-  _________________________________________________
- < Seek your prey first and then go for the attack >
-  -------------------------------------------------
-      \
-       \
-                                   .::!!!!!!!:.
-  .!!!!!:.                        .:!!!!!!!!!!!!
-  ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$ 
-      :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P 
-      $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$# 
-      $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$* 
-      ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R" 
-        "*$bd$$$$      '*$$$$$$$$$$$o+#" 
-             """"          """"""" 
+
+[***]                Host Locator               [***]
+[***]           Created by: C0defire            [***]
+                    Version: 1.0.0
+[***]       Github: Github.com/javierfaustino   [***]
+
+        `-:::-.`                   
+     -ohysooooshy+.                
+   -hy/-:::::::-:+hs.              
+  /do:::///////::-:yd.             
+ -dy+://++++++++:::ohh             
+ +dso+/+/+o+o+/+/:+oyd.            
+ /dsss+oooo+-/+ooossyd`            
+ `hhssoosoo+: -+osssdo             
+  .hhsssooo++  `:oydo              
+    /yhysssos-.:shh+`.`            
+      ./oyyhhyso:` -o+ys-          
+                    .ymdhs:        
+                      .smdhy:      
+                        .smdhy:    
+                          .smdhy.  
+                            .sho/+ 
+                              `/- 
 EOF
-#echo "Available interfaces:"
-#list_IF=$(netstat -i | head | awk {'print $1'} | grep -v "Kernel" | grep -v "Iface")
-#echo $list_IF 
-#echo "Enter interface"
-#read interface
-
 class=$(ip address | grep "inet" | head -3 | tail -1 | awk {'print $2'} | cut -f1-3 -d.)
-
-echo
 #read class
-echo
-echo "########################################"
 echo
 echo -e "\033[1mTargets Found:\033[0m"
 echo
 for x in `seq 1 254`;
 do
-    ping -c 1 $class.$x | grep "bytes from" | cut -d " " -f4 | cut -d ":" -f1 &
+	tput setaf 2
+    tput setaf 2; ping -c 1 $class.$x | grep "bytes from" | cut -d " " -f4 | cut -d ":" -f1 &
+    tput setaf 2
 done
+
+tput sgr0
    
 sleep 4
-echo
-echo  "########################################"
 echo
